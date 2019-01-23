0980bcfa9f76c8d0764d2cd4b11de403ac11c515
Diana Utan <dia_utzi@yahoo.com>
2019-01-23 17:07:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+ 
+  
+function sayHiToGrandma(string) {
+  var lowercase = "hello"
+  // var uppercase = "HELLO";
+  
+ function sayHiToGrandma(string)
+  if ( string.toLowerCase() === lowercase) {
+    return " I can\'t hear you! " }
+}
+    
+    
+    ( string.toUpperCase() === uppercase) {
+      return  " YES INDEED! " 
+     
+   }
+    else if ( string)
+      
+      
+
+}
